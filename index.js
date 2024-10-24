9e43908c7a4ea6c24e4fb643284d6b198f91298e
dannykhan02 <dannykhan614@gmail.com>
2024-10-24 07:04:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Jonte";
+let welcomeMessage = "Welcome to Flatbook,  Jonte!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, JONTE!"
+let shortGreeting = "Welcome, J!"
+
